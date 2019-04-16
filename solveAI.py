fd31ded8b68be2c8a44cd3260c40bcfe9bff4b46
Wu <andywu0913@yahoo.com.tw>
2019-04-15 20:25:57 -0500

    Make all frontiers being stored in set.

--- solveAI.py
+++ solveAI.py
@@ -1,33 +1,33 @@
 import numpy as np
+import random
+import pyautogui
 import tensorflow as tf
 import Board
+import NeuralNetwork
 import cv2
 
 board = Board.Board(resolution_scale=2)
-sess = tf.Session()
+nn = NeuralNetwork.NeuralNetwork()
 
-
-x = tf.placeholder(tf.float32, shape=(None, 8))
-
-
-for x in range(0, 100):
-	while 1:
+for generation in range(0,10):
+	board.update_board_status()
+	if(board.determine_board_status() == 0):
+		random_x, random_y = board.board_position[random.randrange(board.game_row)][random.randrange(board.game_col)]
+		pyautogui.click((board.screen_start_x + random_x) / board.resolution_scale, (board.screen_start_y + random_y) / board.resolution_scale)
 		board.update_board_status()
-		board.check_start_end()
-		for i in range(0, board.game_row):
-			for j in range(0, board.game_col):
-				if 1 <= board.board_status[i][j] <= 8:
-					board.check_surrounding(i, j)
+		print('Start New Game.')
 
-# Unopened [189. 189. 189.]
-# Opened [189. 189. 189.]
-# Flagged [122.296875  118.71875   179.8515625]
-# bombdeath [18.2734375  17.04296875 37.515625  ]
-# 1 [218.68359375 115.4921875  103.37109375]
-# 2 [ 84.9453125 148.4765625  83.1328125]
-# 3 [ 98.4140625  90.7734375 224.15625  ]
-# 4 [144.375     72.046875  66.296875]
-# 5 [ 75.44921875  72.9921875  147.28515625]
-# 6 [143.265625   143.7578125   70.84765625]
-# 7 [109.99609375 109.99609375 109.99609375]
-# 8 [139.90625 139.90625 139.90625]
+	frontier = set()
+	while board.determine_board_status() == 1:
+		print('while')
+		for row in range(0, board.game_row):
+			for col in range(0, board.game_col):
+				if 1 <= board.board_status[row][col] <= 8:
+					for i in range(max(0, row - 1), min(board.game_row, row + 2)):
+						for j in range(max(0, col - 1), min(board.game_col, col + 2)):
+							if i == row and j == col:
+								continue
+							elif board.board_status[i, j] == 0:
+								frontier.add((i, j));
+		print(frontier)
+
